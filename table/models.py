3fbeb4149aa8fbe0586653f30d83f87d3646f38b
IvanPLebedev <ivan.p.lebedev@yandex.ru>
2020-01-08 15:41:22 +0400

    commit database ver 0.1

--- table/models.py
+++ table/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+class Status(models.Model):
+    title = models.CharField(max_length = 50)
 
+    def __str__(self):
+        return str(self.title)
+        
+    
+class Task(models.Model):
+    status = models.ForeignKey(Status, on_delete = models.CASCADE)
+    title = models.CharField(max_length = 300)
+    
+    def __str__(self):
+        if len(str(self.title)) < 50:
+            return str(self.title)
+        else:
+            return str(self.title)[:50] + "..."
