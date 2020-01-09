5b32fcec64ed910cff72a7bd985c908f38a68cbc
IvanPLebedev <ivan.p.lebedev@yandex.ru>
2020-01-09 19:29:53 +0400

    commit добавлено главное представление

--- table/urls.py
+++ table/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 app_name = 'table'
 urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('tasks/', views.tasks,  name = 'tasks'),
 ]
--- table/views.py
+++ table/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Status, Task
 
-# Create your views here.
+def index(request):
+	return render(request, 'table/index.html')
+
+def tasks(request):
+    statuses = Status.objects.order_by('id')
+    tasks = Task.objects.order_by('id')
+    context = {'statuses': statuses, 'tasks': tasks}
+    return render(request, 'table/tasks.html', context)
